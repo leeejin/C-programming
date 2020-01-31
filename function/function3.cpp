3c894abd3bdc179bf9e31143e9613a7ea805b090
leeejin <asd72621@naver.com>
2020-01-31 23:27:55 +0900

    c++

---
+++ function/function3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath> //수학에 관련한 함수준비 c:crt, math:math 
+using namespace std;
+int main()
+{
+	double sqrt_2 = sqrt(2.0);
+	double pow_12_2 = pow(12,2);
+	cout<<"2의 제곱근 = "<<sqrt_2<<"\n";
+	cout<<"12의 제곱 = "<<pow_12_2<<"\n";
+	return 0;
+}
